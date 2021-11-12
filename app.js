59a08761cf29f7b8816b592eb450a0e3f1064d64
stevan <stevanparisian@gmail.com>
2021-11-12 20:12:38 +0100

    instalation mongodb

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        userId: 'identifiant MongoDB',
-        name: 'nom de la sauce',
-        manufacturer: 'fabricant de la sauce',
-        description: 'description de la sauce',
-        mainPepper: 'le principal ingrédient épicé de la sauce',
-        imageUrl: 'URL image de la sauce téléchargée par utilisateur',
-        heat: 'nombre entre 1 et 10 décrivant la sauce',
-        likes: 'nombre utilisateurs qui aiment (= likent) la sauce',
-        dislikes:'nombre utilisateurs qui naiment pas (= dislike) la sauce',
-        usersLiked:'tableau des identifiants des utilisateurs qui ont aimé (= liked) la sauce',
-        usersDisliked:'tableau des identifiants des utilisateurs qui nont pas aimé (= disliked) la sauce',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
 
-module.exports = app; 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+mongoose.connect('mongodb+srv://stevanparisian:Nick-cave-1982!@cluster0.ruejc.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
+
+app.use('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
+});
+
+module.exports = app;
