366963bbb7688fce7a8f919c391fbf2f4a8e7c20
stevan <stevanparisian@gmail.com>
2021-11-13 17:05:13 +0100

    créez des utilisateurs

--- models/User.js
+++ models/User.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: { type: string, required: true, unique: true },
-    password: { type: string, required: true}
-
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
 userSchema.plugin(uniqueValidator);
