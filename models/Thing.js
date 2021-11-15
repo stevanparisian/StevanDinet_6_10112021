87c75f154fd48493dfab4cb162ef041194435420
stevan <stevanparisian@gmail.com>
2021-11-15 13:56:19 +0100

    creation multer

--- models/Thing.js
+++ models/Thing.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
 const thingSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
   userId: { type: String, required: true },
-  price: { type: Number, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: String, required: true },
+  likes: { type: String, required: true },
+  dislikes: { type: String, required: true },
+  usersLiked: { type: String, required: true },
+  usersDisliked: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Thing', thingSchema);
